e1fe8595f4b1831d3da8f3615109b048c1c8b6bc
RuslanUsmanov <usmanovruslan322@gmail.com>
2024-07-14 15:36:33 +0500

    Добавлена начальная логика работы приложения

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+import os
 import sys
 
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import (
+    QApplication,
+    QErrorMessage,
+    QFileDialog,
+    QMainWindow,
+)
 
+from core import service
 from ui.src.ui_mainwindow import Ui_MainWindow
 
 
@@ -10,6 +17,41 @@ class TrasformatorDesomposer(QMainWindow):
         super(TrasformatorDesomposer, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+
+        self.dHandler = service.DataHandler()
+
+        self.ui.select_files_pushbutton.clicked.connect(
+            self.open_select_files_dialog
+        )
+
+    def open_select_files_dialog(self):
+        filenames, _ = QFileDialog.getOpenFileName(
+            self,
+            caption="Выберите файлы",
+            filter="Файл данных (*.txt *.csv *.dat)",
+            dir=f"{os.getenv("HOME")}",
+        )
+        if not filenames:
+            return
+
+        try:
+            self.dHandler.read_from_file(filenames)
+        except Exception as ex:
+            msg_box = QErrorMessage(self)
+            msg_box.setWindowTitle("Ошибка")
+            msg_box.showMessage(f"Ошибка во время чтения файла:\n{repr(ex)}")
+            return
+
+        self.update_coeffs()
+
+    def update_coeffs(self):
+        popt = self.dHandler.get_coefficients()
+        self.ui.c1_lineedit.setText(str(popt[0]))
+        self.ui.a1_lineedit.setText(str(popt[1]))
+        self.ui.c2_lineedit.setText(str(popt[2]))
+        self.ui.a2_lineedit.setText(str(popt[3]))
+        self.ui.c3_lineedit.setText(str(popt[4]))
+        self.ui.a3_lineedit.setText(str(popt[5]))
 
 
 if __name__ == "__main__":
