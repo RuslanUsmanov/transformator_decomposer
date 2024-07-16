98f22babef953feefe6c5c90dcfcace444093258
RuslanUsmanov <usmanovruslan322@gmail.com>
2024-07-16 23:47:25 +0500

    Добавлены валидаторы

--- ui/src/ui_passport_params.py
+++ ui/src/ui_passport_params.py
@@ -1,30 +1,33 @@
 # -*- coding: utf-8 -*-
 
-################################################################################
-## Form generated from reading UI file 'ui_passport_params.ui'
-##
-## Created by: Qt User Interface Compiler version 6.7.2
-##
-## WARNING! All changes made in this file will be lost when recompiling UI file!
-################################################################################
+###############################################################################
+# Form generated from reading UI file 'ui_passport_params.ui'
+#
+# Created by: Qt User Interface Compiler version 6.7.2
+#
+# WARNING! All changes made in this file will be lost when recompiling UI file!
+###############################################################################
 
-from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
-    QMetaObject, QObject, QPoint, QRect,
-    QSize, QTime, QUrl, Qt)
-from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
-    QFont, QFontDatabase, QGradient, QIcon,
-    QImage, QKeySequence, QLinearGradient, QPainter,
-    QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QAbstractButton, QApplication, QDialog, QDialogButtonBox,
-    QGridLayout, QLabel, QLineEdit, QSizePolicy,
-    QVBoxLayout, QWidget)
+from PySide6.QtCore import QCoreApplication, QMetaObject, QSize, Qt
+from PySide6.QtGui import QDoubleValidator, QFont, QIntValidator
+from PySide6.QtWidgets import (
+    QDialogButtonBox,
+    QGridLayout,
+    QLabel,
+    QLineEdit,
+    QSizePolicy,
+    QVBoxLayout,
+)
+
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         if not Dialog.objectName():
-            Dialog.setObjectName(u"Dialog")
+            Dialog.setObjectName("Dialog")
         Dialog.resize(375, 230)
-        sizePolicy = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Preferred)
+        sizePolicy = QSizePolicy(
+            QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Preferred
+        )
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(Dialog.sizePolicy().hasHeightForWidth())
@@ -35,84 +38,125 @@ class Ui_Dialog(object):
         font.setPointSize(12)
         Dialog.setFont(font)
         Dialog.setModal(True)
+
+        self.only_double = QDoubleValidator()
+        self.only_int = QIntValidator()
+
         self.verticalLayout = QVBoxLayout(Dialog)
-        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setObjectName("verticalLayout")
         self.gridLayout = QGridLayout()
-        self.gridLayout.setObjectName(u"gridLayout")
+        self.gridLayout.setObjectName("gridLayout")
         self.label = QLabel(Dialog)
-        self.label.setObjectName(u"label")
+        self.label.setObjectName("label")
 
         self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
 
         self.type_lineedit = QLineEdit(Dialog)
-        self.type_lineedit.setObjectName(u"type_lineedit")
+        self.type_lineedit.setObjectName("type_lineedit")
 
         self.gridLayout.addWidget(self.type_lineedit, 0, 1, 1, 1)
 
         self.label_2 = QLabel(Dialog)
-        self.label_2.setObjectName(u"label_2")
+        self.label_2.setObjectName("label_2")
 
         self.gridLayout.addWidget(self.label_2, 1, 0, 1, 1)
 
         self.power_lineedit = QLineEdit(Dialog)
-        self.power_lineedit.setObjectName(u"power_lineedit")
+        self.power_lineedit.setObjectName("power_lineedit")
+        self.power_lineedit.setValidator(self.only_double)
 
         self.gridLayout.addWidget(self.power_lineedit, 1, 1, 1, 1)
 
         self.label_3 = QLabel(Dialog)
-        self.label_3.setObjectName(u"label_3")
+        self.label_3.setObjectName("label_3")
 
         self.gridLayout.addWidget(self.label_3, 2, 0, 1, 1)
 
         self.u_lineedit = QLineEdit(Dialog)
-        self.u_lineedit.setObjectName(u"u_lineedit")
+        self.u_lineedit.setObjectName("u_lineedit")
+        self.u_lineedit.setValidator(self.only_double)
 
         self.gridLayout.addWidget(self.u_lineedit, 2, 1, 1, 1)
 
         self.label_4 = QLabel(Dialog)
-        self.label_4.setObjectName(u"label_4")
+        self.label_4.setObjectName("label_4")
 
         self.gridLayout.addWidget(self.label_4, 3, 0, 1, 1)
 
         self.num_phases_lineedit = QLineEdit(Dialog)
-        self.num_phases_lineedit.setObjectName(u"num_phases_lineedit")
+        self.num_phases_lineedit.setObjectName("num_phases_lineedit")
+        self.num_phases_lineedit.setValidator(self.only_int)
 
         self.gridLayout.addWidget(self.num_phases_lineedit, 3, 1, 1, 1)
 
         self.label_5 = QLabel(Dialog)
-        self.label_5.setObjectName(u"label_5")
+        self.label_5.setObjectName("label_5")
 
         self.gridLayout.addWidget(self.label_5, 4, 0, 1, 1)
 
         self.material_lineedit = QLineEdit(Dialog)
-        self.material_lineedit.setObjectName(u"material_lineedit")
+        self.material_lineedit.setObjectName("material_lineedit")
 
         self.gridLayout.addWidget(self.material_lineedit, 4, 1, 1, 1)
-
 
         self.verticalLayout.addLayout(self.gridLayout)
 
         self.buttonBox = QDialogButtonBox(Dialog)
-        self.buttonBox.setObjectName(u"buttonBox")
+        self.buttonBox.setObjectName("buttonBox")
         self.buttonBox.setOrientation(Qt.Orientation.Horizontal)
-        self.buttonBox.setStandardButtons(QDialogButtonBox.StandardButton.Cancel|QDialogButtonBox.StandardButton.Ok)
+        self.buttonBox.setStandardButtons(
+            QDialogButtonBox.StandardButton.Cancel
+            | QDialogButtonBox.StandardButton.Ok
+        )
 
         self.verticalLayout.addWidget(self.buttonBox)
-
 
         self.retranslateUi(Dialog)
         self.buttonBox.accepted.connect(Dialog.accept)
         self.buttonBox.rejected.connect(Dialog.reject)
 
         QMetaObject.connectSlotsByName(Dialog)
+
     # setupUi
 
     def retranslateUi(self, Dialog):
-        Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"\u041f\u0430\u0441\u043f\u043e\u0440\u0442\u043d\u044b\u0435 \u0434\u0430\u043d\u043d\u044b\u0435", None))
-        self.label.setText(QCoreApplication.translate("Dialog", u"\u0422\u0438\u043f", None))
-        self.label_2.setText(QCoreApplication.translate("Dialog", u"\u041c\u043e\u0449\u043d\u043e\u0441\u0442\u044c", None))
-        self.label_3.setText(QCoreApplication.translate("Dialog", u"\u041d\u0430\u043f\u0440\u044f\u0436\u0435\u043d\u0438\u0435 (U1/U2)", None))
-        self.label_4.setText(QCoreApplication.translate("Dialog", u"\u041a\u043e\u043b\u0438\u0447\u0435\u0441\u0442\u0432\u043e \u0444\u0430\u0437", None))
-        self.label_5.setText(QCoreApplication.translate("Dialog", u"\u041c\u0430\u0442\u0435\u0440\u0438\u0430\u043b \u043e\u0431\u043c\u043e\u0442\u043a\u0438", None))
-    # retranslateUi
+        Dialog.setWindowTitle(
+            QCoreApplication.translate(
+                "Dialog",
+                "\u041f\u0430\u0441\u043f\u043e\u0440\u0442\u043d\u044b\u0435 \u0434\u0430\u043d\u043d\u044b\u0435",  # noqa: E501
+                None,
+            )
+        )
+        self.label.setText(
+            QCoreApplication.translate("Dialog", "\u0422\u0438\u043f", None)
+        )
+        self.label_2.setText(
+            QCoreApplication.translate(
+                "Dialog",
+                "\u041c\u043e\u0449\u043d\u043e\u0441\u0442\u044c",
+                None,
+            )
+        )
+        self.label_3.setText(
+            QCoreApplication.translate(
+                "Dialog",
+                "\u041d\u0430\u043f\u0440\u044f\u0436\u0435\u043d\u0438\u0435 (U1/U2)",  # noqa: E501
+                None,
+            )
+        )
+        self.label_4.setText(
+            QCoreApplication.translate(
+                "Dialog",
+                "\u041a\u043e\u043b\u0438\u0447\u0435\u0441\u0442\u0432\u043e \u0444\u0430\u0437",  # noqa: E501
+                None,
+            )
+        )
+        self.label_5.setText(
+            QCoreApplication.translate(
+                "Dialog",
+                "\u041c\u0430\u0442\u0435\u0440\u0438\u0430\u043b \u043e\u0431\u043c\u043e\u0442\u043a\u0438",  # noqa: E501
+                None,
+            )
+        )
 
+    # retranslateUi
--- ui/src/ui_source_params.py
+++ ui/src/ui_source_params.py
@@ -1,30 +1,33 @@
 # -*- coding: utf-8 -*-
 
-################################################################################
-## Form generated from reading UI file 'ui_source_params.ui'
-##
-## Created by: Qt User Interface Compiler version 6.7.2
-##
-## WARNING! All changes made in this file will be lost when recompiling UI file!
-################################################################################
+###############################################################################
+# Form generated from reading UI file 'ui_source_params.ui'
+#
+# Created by: Qt User Interface Compiler version 6.7.2
+#
+# WARNING! All changes made in this file will be lost when recompiling UI file!
+###############################################################################
 
-from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
-    QMetaObject, QObject, QPoint, QRect,
-    QSize, QTime, QUrl, Qt)
-from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
-    QFont, QFontDatabase, QGradient, QIcon,
-    QImage, QKeySequence, QLinearGradient, QPainter,
-    QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QAbstractButton, QApplication, QDialog, QDialogButtonBox,
-    QGridLayout, QLabel, QLineEdit, QSizePolicy,
-    QVBoxLayout, QWidget)
+from PySide6.QtCore import QCoreApplication, QMetaObject, QSize, Qt
+from PySide6.QtGui import QDoubleValidator, QFont
+from PySide6.QtWidgets import (
+    QDialogButtonBox,
+    QGridLayout,
+    QLabel,
+    QLineEdit,
+    QSizePolicy,
+    QVBoxLayout,
+)
+
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         if not Dialog.objectName():
-            Dialog.setObjectName(u"Dialog")
+            Dialog.setObjectName("Dialog")
         Dialog.resize(220, 170)
-        sizePolicy = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
+        sizePolicy = QSizePolicy(
+            QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed
+        )
         sizePolicy.setHorizontalStretch(220)
         sizePolicy.setVerticalStretch(170)
         sizePolicy.setHeightForWidth(Dialog.sizePolicy().hasHeightForWidth())
@@ -35,68 +38,96 @@ class Ui_Dialog(object):
         Dialog.setFont(font)
         Dialog.setSizeGripEnabled(False)
         Dialog.setModal(True)
+
+        self.only_double = QDoubleValidator()
+
         self.verticalLayout = QVBoxLayout(Dialog)
-        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setObjectName("verticalLayout")
         self.gridLayout = QGridLayout()
-        self.gridLayout.setObjectName(u"gridLayout")
+        self.gridLayout.setObjectName("gridLayout")
         self.label = QLabel(Dialog)
-        self.label.setObjectName(u"label")
+        self.label.setObjectName("label")
 
         self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
 
         self.r_meas_lineedit = QLineEdit(Dialog)
-        self.r_meas_lineedit.setObjectName(u"r_meas_lineedit")
-        self.r_meas_lineedit.setInputMethodHints(Qt.InputMethodHint.ImhPreferNumbers)
+        self.r_meas_lineedit.setObjectName("r_meas_lineedit")
+        self.r_meas_lineedit.setInputMethodHints(
+            Qt.InputMethodHint.ImhPreferNumbers
+        )
+        self.r_meas_lineedit.setValidator(self.only_double)
 
         self.gridLayout.addWidget(self.r_meas_lineedit, 0, 1, 1, 1)
 
         self.label_2 = QLabel(Dialog)
-        self.label_2.setObjectName(u"label_2")
+        self.label_2.setObjectName("label_2")
 
         self.gridLayout.addWidget(self.label_2, 1, 0, 1, 1)
 
         self.k_u_lineedit = QLineEdit(Dialog)
-        self.k_u_lineedit.setObjectName(u"k_u_lineedit")
-        self.k_u_lineedit.setInputMethodHints(Qt.InputMethodHint.ImhPreferNumbers)
+        self.k_u_lineedit.setObjectName("k_u_lineedit")
+        self.k_u_lineedit.setInputMethodHints(
+            Qt.InputMethodHint.ImhPreferNumbers
+        )
+        self.k_u_lineedit.setValidator(self.only_double)
 
         self.gridLayout.addWidget(self.k_u_lineedit, 1, 1, 1, 1)
 
         self.label_3 = QLabel(Dialog)
-        self.label_3.setObjectName(u"label_3")
+        self.label_3.setObjectName("label_3")
 
         self.gridLayout.addWidget(self.label_3, 2, 0, 1, 1)
 
         self.r_one_lineedit = QLineEdit(Dialog)
-        self.r_one_lineedit.setObjectName(u"r_one_lineedit")
-        self.r_one_lineedit.setInputMethodHints(Qt.InputMethodHint.ImhPreferNumbers)
+        self.r_one_lineedit.setObjectName("r_one_lineedit")
+        self.r_one_lineedit.setInputMethodHints(
+            Qt.InputMethodHint.ImhPreferNumbers
+        )
+        self.r_one_lineedit.setValidator(self.only_double)
 
         self.gridLayout.addWidget(self.r_one_lineedit, 2, 1, 1, 1)
-
 
         self.verticalLayout.addLayout(self.gridLayout)
 
         self.buttonBox = QDialogButtonBox(Dialog)
-        self.buttonBox.setObjectName(u"buttonBox")
+        self.buttonBox.setObjectName("buttonBox")
         self.buttonBox.setOrientation(Qt.Orientation.Horizontal)
-        self.buttonBox.setStandardButtons(QDialogButtonBox.StandardButton.Cancel|QDialogButtonBox.StandardButton.Ok)
+        self.buttonBox.setStandardButtons(
+            QDialogButtonBox.StandardButton.Cancel
+            | QDialogButtonBox.StandardButton.Ok
+        )
 
         self.verticalLayout.addWidget(self.buttonBox)
-
 
         self.retranslateUi(Dialog)
         self.buttonBox.accepted.connect(Dialog.accept)
         self.buttonBox.rejected.connect(Dialog.reject)
 
         QMetaObject.connectSlotsByName(Dialog)
+
     # setupUi
 
     def retranslateUi(self, Dialog):
-        Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"\u0418\u0441\u0445\u043e\u0434\u043d\u044b\u0435 \u0434\u0430\u043d\u043d\u044b\u0435", None))
-        self.label.setText(QCoreApplication.translate("Dialog", u"R\u0438\u0437\u043c", None))
-        self.r_meas_lineedit.setText(QCoreApplication.translate("Dialog", u"352.8", None))
-        self.label_2.setText(QCoreApplication.translate("Dialog", u"Ku", None))
-        self.k_u_lineedit.setText(QCoreApplication.translate("Dialog", u"2000", None))
-        self.label_3.setText(QCoreApplication.translate("Dialog", u"R1", None))
-        self.r_one_lineedit.setText(QCoreApplication.translate("Dialog", u"0.683", None))
-    # retranslateUi
+        Dialog.setWindowTitle(
+            QCoreApplication.translate(
+                "Dialog",
+                "\u0418\u0441\u0445\u043e\u0434\u043d\u044b\u0435 \u0434\u0430\u043d\u043d\u044b\u0435",  # noqa: E501
+                None,
+            )
+        )
+        self.label.setText(
+            QCoreApplication.translate("Dialog", "R\u0438\u0437\u043c", None)
+        )
+        self.r_meas_lineedit.setText(
+            QCoreApplication.translate("Dialog", "352.8", None)
+        )
+        self.label_2.setText(QCoreApplication.translate("Dialog", "Ku", None))
+        self.k_u_lineedit.setText(
+            QCoreApplication.translate("Dialog", "2000", None)
+        )
+        self.label_3.setText(QCoreApplication.translate("Dialog", "R1", None))
+        self.r_one_lineedit.setText(
+            QCoreApplication.translate("Dialog", "0.683", None)
+        )
 
+    # retranslateUi
