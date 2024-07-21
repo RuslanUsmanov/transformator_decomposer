2d67b5c78ffbf3c8ff2c4d1152fac91a09fa22d9
RuslanUsmanov <usmanovruslan322@gmail.com>
2024-07-22 01:05:03 +0500

    func теперь публичная

--- core/decompose.py
+++ core/decompose.py
@@ -20,7 +20,7 @@ def get_coefficients(t: np.array, y: np.array) -> np.ndarray[np.float64]:
         Массив коэффициентов целевой функции.
     """
     popt, _ = curve_fit(
-        f=_func,
+        f=func,
         xdata=t,
         ydata=y,
         p0=(2, 0, 2, 0, 2, 0),
@@ -41,19 +41,19 @@ def get_metrics(
     Returns:
         Словарь с метриками.
     """
-    mae = mean_absolute_error(y, _func(t, *popt))
-    mape = mean_absolute_percentage_error(y, _func(t, *popt))
-    mdae = median_absolute_error(y, _func(t, *popt))
-    mse = mean_squared_error(y, _func(t, *popt))
-    r2 = r2_score(y, _func(t, *popt))
+    mae = mean_absolute_error(y, func(t, *popt))
+    mape = mean_absolute_percentage_error(y, func(t, *popt))
+    mdae = median_absolute_error(y, func(t, *popt))
+    mse = mean_squared_error(y, func(t, *popt))
+    r2 = r2_score(y, func(t, *popt))
 
     return {"mae": mae, "mape": mape, "mdae": mdae, "mse": mse, "r2": r2}
 
 
-def _func(t, c_1, a_1, c_2, a_2, c_3, a_3):
+def func(t, c_1, a_1, c_2, a_2, c_3, a_3):
     """Моделируемая функция - сумма трех экспонент."""
     return (
-        np.multiply(c_1, np.exp(a_1 * t))
-        + np.multiply(c_2, np.exp(a_2 * t))
-        + np.multiply(c_3, np.exp(a_3 * t))
+        np.multiply(c_1, np.exp(-a_1 * t))
+        + np.multiply(c_2, np.exp(-a_2 * t))
+        + np.multiply(c_3, np.exp(-a_3 * t))
     )
